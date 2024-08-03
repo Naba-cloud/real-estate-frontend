c5a2381b5a1169c17251365cb03b2b833861a56b
Naba-cloud <nabazehra45@gmail.com>
2024-08-03 16:24:38 +0500

    design[ui/ux]:added property cards for different screens

--- src/sections/PropertySection/PropertySection.jsx
+++ src/sections/PropertySection/PropertySection.jsx
@@ -4,7 +4,7 @@ import { Grid } from '@mui/material'
 const PropertySection = () => {
  return(<Grid container spacing={3}>
     {[1,2,3,4,5,6,7,8,9,10,11,12].map(()=>(
-  <Grid item xs={6} lg={4} md={6}>
+  <Grid item xs={12} xl={3} md={4}>
   <PropertyCard/></Grid>))}</Grid>)
 }
 
